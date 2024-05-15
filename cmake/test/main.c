48ca16f9f653983adce043fe022cdb28287bf24c
Ozkan Sezer <sezeroz@gmail.com>
2024-05-15 23:56:56 +0300

    fix cmake/test/main.c for new version api

--- cmake/test/main.c
+++ cmake/test/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-    const SDL_Version *v;
+    int version;
 
     if (SDL_Init(0) < 0) {
         SDL_Log("SDL_Init: could not initialize SDL: %s", SDL_GetError());
@@ -16,8 +16,11 @@ int main(int argc, char *argv[])
     if (TTF_Init() == -1) {
         SDL_Log("TTF_Init: %s", TTF_GetError());
     }
-    v = RTF_Linked_Version();
-    SDL_Log("SDL_rtf linked version: %u.%u.%u", v->major, v->minor, v->patch);
+    v = RTF_Version();
+    SDL_Log("SDL_rtf linked version: %u.%u.%u",
+                SDL_VERSIONNUM_MAJOR(version),
+                SDL_VERSIONNUM_MINOR(version),
+                SDL_VERSIONNUM_MICRO(version));
 
     TTF_Quit();
     SDL_Quit();
