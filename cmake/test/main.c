92cd9fc4bbf238da48b64c0662977091a28d5843
Ozkan Sezer <sezeroz@gmail.com>
2024-08-27 22:32:20 +0300

    cmake/test/main.c: Updated for SDL3 change to use SDL_bool return.

--- cmake/test/main.c
+++ cmake/test/main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     (void)argv;
     int version;
 
-    if (SDL_Init(0) < 0) {
+    if (!SDL_Init(0)) {
         SDL_Log("SDL_Init: could not initialize SDL: %s", SDL_GetError());
         return 1;
     }
